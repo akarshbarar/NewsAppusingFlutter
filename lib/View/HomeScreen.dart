d19614e94e32965e2384118fe9c586417deb79c8
Akarsh Barar <barar.akarsh8@gmail.com>
2021-09-27 22:58:27 +0530

    Added api call

--- lib/View/HomeScreen.dart
+++ lib/View/HomeScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,27 +11,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Future<List> getData() async {
+    var data = await http
+        .get(Uri.parse("https://saurav.tech/NewsAPI/everything/cnn.json"));
+    print(jsonDecode(data.body)["articles"]);
+    return jsonDecode(data.body)["articles"];
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
             appBar: AppBar(
               title: Text("New App"),
+              centerTitle: true,
+              elevation: 0,
             ),
-            body: ListView.builder(
-                itemCount: 100,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        Image.network(
-                            "https://cdn.cnn.com/cnnnext/dam/assets/210730111329-20210730-japan-chisako-kakehi-black-widow-super-tease.jpg"),
-                        Text("Title"),
-                        Text("data")
-                      ],
-                    ),
-                  );
+            body: FutureBuilder<List>(
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: EdgeInsets.all(10),
+                            child: Column(
+                              children: [
+                                Image.network(
+                                  snapshot.data![index]["urlToImage"],
+                                ),
+                                Text(
+                                  snapshot.data![index]["title"],
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                Text(snapshot.data![index]["description"])
+                              ],
+                            ),
+                          );
+                        });
+                  }
+                  return CircularProgressIndicator();
                 })));
   }
 }
